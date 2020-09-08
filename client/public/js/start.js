c0b1728c5fcec7e4417f81891c170bae84d76981
Moritz Vogel <moritzv7@googlemail.com>
2020-09-08 13:10:16 +0200

    fix(start): height of speechbubbles container on mobile, fade effect

--- client/public/js/start.js
+++ client/public/js/start.js
@@ -8,6 +8,9 @@ function start_nextReview() {
         speechbubble.classList.remove("start-review-visible");
 
     speechbubbles[start_currentReview].classList.add("start-review-visible");
+    speechbubbles[start_currentReview].style.display = "block";
+    setTimeout(speechbubble => speechbubble.style.removeProperty("display"), 500, speechbubbles[start_currentReview - 1]);
+    reviews_container.style.height = speechbubbles[start_currentReview].clientHeight + "px";
 
     start_currentReview = (start_currentReview + 1) % speechbubbles.length;
 }
