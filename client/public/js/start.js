4c9899f0f48555ebdb8ea435cfdcbe1520d98f08
Moritz Vogel <moritzv7@googlemail.com>
2020-09-08 13:25:38 +0200

    fix(start): last speechbubble stayed visible

--- client/public/js/start.js
+++ client/public/js/start.js
@@ -9,7 +9,7 @@ function start_nextReview() {
 
     speechbubbles[start_currentReview].classList.add("start-review-visible");
     speechbubbles[start_currentReview].style.display = "block";
-    setTimeout(speechbubble => speechbubble.style.removeProperty("display"), 500, speechbubbles[start_currentReview - 1]);
+    setTimeout(speechbubble => speechbubble.style.removeProperty("display"), 500, speechbubbles[start_currentReview == 0 ? speechbubbles.length - 1 : start_currentReview - 1]);
     reviews_container.style.height = speechbubbles[start_currentReview].clientHeight + "px";
 
     start_currentReview = (start_currentReview + 1) % speechbubbles.length;
