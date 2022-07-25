dbe5371d7d607e7b39ad4d3a0517e11bae6f0b02
Moritz Vogel <moritzv7@googlemail.com>
2022-07-25 23:32:55 +0200

    feat: apply accent color in workshop overview

--- server/workshops.js
+++ server/workshops.js
@@ -54,6 +54,7 @@ export function getWorkshops(loggedIn, page) {
         }
         if (!workshop.img.includes("/"))
             workshop.img = `${utils.base_url}/api/upload/${workshop.img}`;
+        workshop.textColor = calcTextColor(workshop.color);
     }
     return workshops;
 }
@@ -149,4 +150,16 @@ export function editWorkshop(workshop) {
 
 export function deleteWorkshop(id) {
     db.run(`DELETE FROM workshop WHERE id = '${id}'`);
+}
+
+// Duplicate code from client/public/js/workshop.js
+function calcTextColor(backgroundColor) {
+    const r = parseInt(backgroundColor.substr(1, 2), 16);
+    const g = parseInt(backgroundColor.substr(3, 2), 16);
+    const b = parseInt(backgroundColor.substr(5, 2), 16);
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    if (luminance > 0.5)
+        return "#000000";
+    else
+        return "#ffffff";
 }
