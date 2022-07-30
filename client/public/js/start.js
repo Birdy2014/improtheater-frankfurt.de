0baa8cb5806a886c7a278b3dd657c5fe6dd8dde1
Moritz Vogel <moritzv7@googlemail.com>
2022-07-30 23:08:01 +0200

    chore: use more flexboxes instead of float, fist review visible without javascript

--- client/public/js/start.js
+++ client/public/js/start.js
@@ -1,4 +1,4 @@
-let start_currentReview = 0;
+let start_currentReview = 1;
 
 function start_nextReview() {
     const reviews_container = document.getElementById("start-reviews");
@@ -8,12 +8,8 @@ function start_nextReview() {
         speechbubble.classList.remove("start-review-visible");
 
     speechbubbles[start_currentReview].classList.add("start-review-visible");
-    speechbubbles[start_currentReview].style.display = "block";
-    setTimeout(speechbubble => speechbubble.style.removeProperty("display"), 500, speechbubbles[start_currentReview == 0 ? speechbubbles.length - 1 : start_currentReview - 1]);
-    reviews_container.style.height = speechbubbles[start_currentReview].clientHeight + "px";
 
     start_currentReview = (start_currentReview + 1) % speechbubbles.length;
 }
 
-start_nextReview();
 setInterval(start_nextReview, 10000);
