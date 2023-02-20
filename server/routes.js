4bbd4f2f2ecb77bd1183c8bbeebf9b67fd5afb38
Moritz Vogel <moritzv7@googlemail.com>
2023-02-21 00:08:13 +0100

    fix: replace deprecated substr with slice

--- server/routes.js
+++ server/routes.js
@@ -33,7 +33,7 @@ const routes = [
 
 // Redirect trailing slashes
 router.use(function (req, res, next) {
-    if (req.path.substr(-1) == '/' && req.path.length > 1) {
+    if (req.path.slice(-1) == '/' && req.path.length > 1) {
         let query = req.url.slice(req.path.length);
         res.redirect(301, req.path.slice(0, -1) + query);
     } else {
