b60ea1eccfa1026186a4b1c04ce02692829686bf
Moritz Vogel <moritzv7@gmail.com>
2025-10-23 23:19:30 +0200

    Integrate the analytics tool Umami

--- server/routes.js
+++ server/routes.js
@@ -39,7 +39,7 @@ router.use(function (req, res, next) {
 
 // Set security headers
 router.use((_, res, next) => {
-    res.set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' https://improglycerin.de; frame-ancestors https://improglycerin.de;");
+    res.set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://analytics.improglycerin.de; img-src 'self' https://improglycerin.de; frame-ancestors https://improglycerin.de;");
     next();
 })
 
