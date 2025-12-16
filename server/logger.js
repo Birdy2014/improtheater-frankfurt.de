cc3864d47c9eca850b3a33a6a4261df336eb7dcb
Moritz Vogel <moritzv7@gmail.com>
2025-12-16 19:43:25 +0100

    logger: Add formatting for systemd-journald

--- server/logger.js
+++ server/logger.js
@@ -1,11 +1,29 @@
-export async function error(message) {
-    console.error("\x1b[1;31mERROR: %s\x1b[0m", message)
+import { ppid } from "process";
+
+function formatJournald(level, message) {
+    return message.split("\n").map(s => `<${level}>${s}`).join("\n")
 }
 
-export async function warn(message) {
-    console.warn("\x1b[0;33mWARN: %s\x1b[0m", message)
+export function error(message) {
+    if (ppid === 1) {
+        console.error(formatJournald(3, message))
+    } else {
+        console.error("\x1b[1;31m%s\x1b[0m", message)
+    }
 }
 
-export async function info(message) {
-    console.info(message)
+export function warn(message) {
+    if (ppid === 1) {
+        console.warn(formatJournald(4, message))
+    } else {
+        console.warn("\x1b[0;33m%s\x1b[0m", message)
+    }
+}
+
+export function info(message) {
+    if (ppid === 1) {
+        console.info(formatJournald(6, message))
+    } else {
+        console.info(message)
+    }
 }
